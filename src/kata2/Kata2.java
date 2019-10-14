167c009a5b750ae27c2368d102dfac22aa19a539
carvsk <carvsk@LAPTOP-IJ1BED1T>
2019-10-14 01:24:49 +0100

    Kata2 versión 4

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -7,7 +7,7 @@ public class Kata2 {
 
     public static void main(String[] args) {
     
-        int[] data = {1, -10, 25, 898, 0, -456, 2, 11, 98, 345, 2, -24, -7890, 7, 99};
+        Integer[] data = {1, -10, 25, 898, 0, -456, 2, 11, 98, 345, 2, -24, -7890, 7, 99};
         Histogram histo = new Histogram(data);
         Map<Integer, Integer> histogr = histo.getHistogram();
         for (Map.Entry<Integer, Integer> entry : histogr.entrySet()) {
